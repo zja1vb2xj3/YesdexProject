a554d2594cdd206599a6657b502a8a2b981261bb
PDG <zja1vb2xj3@naver.com>
2017-10-10 23:15:16 +0900

    MapInfoActivity 진행중

--- app/src/main/java/com/android/beaconyx/yesdexproject/Activity/MapInfoActivity.java
+++ app/src/main/java/com/android/beaconyx/yesdexproject/Activity/MapInfoActivity.java
@@ -1,15 +1,43 @@
 package com.android.beaconyx.yesdexproject.Activity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Point;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageView;
 
+import com.android.beaconyx.yesdexproject.CustomView.CustomMapView;
 import com.android.beaconyx.yesdexproject.R;
 
 public class MapInfoActivity extends AppCompatActivity {
-//
+
+    private Point mDrawScreenPosition;
+    private CustomMapView mMapImageView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mapinfo);
+
+        mMapImageView = (CustomMapView) findViewById(R.id.mapImage);
+
+        mMapImageView.measure(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.MATCH_PARENT);
+
+        int width = mMapImageView.getMeasuredWidth();
+        int height = mMapImageView.getMeasuredHeight();
+
+
     }
+
+
+
+
 }
---
+++ app/src/main/java/com/android/beaconyx/yesdexproject/CustomView/CustomMapView.java
@@ -0,0 +1,42 @@
+package com.android.beaconyx.yesdexproject.CustomView;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.android.beaconyx.yesdexproject.R;
+
+/**
+ * Created by user on 2017-10-10.
+ */
+
+public class CustomMapView extends ImageView {
+
+    private int mMapWidth;
+    private int mMapHeight;
+
+    public CustomMapView(Context context) {
+        super(context);
+    }
+
+    public CustomMapView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        this.setBackgroundResource(R.drawable.sample_map_img);
+
+        Bitmap marker = Bitmap.createBitmap(100, 100, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(marker);
+        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+
+        paint.setColor(Color.BLACK);
+        canvas.drawCircle(50,50,10, paint);
+    }
+
+
+}
