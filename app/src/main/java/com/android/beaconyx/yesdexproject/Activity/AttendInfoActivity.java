76e9d33b55a78fb87d72cd403b9c13b96287b6a3
박동관 <zja1vb2xj3@naver.com>
2017-10-12 14:09:18 +0900

    기능수정

--- app/src/main/java/com/android/beaconyx/yesdexproject/Activity/AttendInfoActivity.java
+++ app/src/main/java/com/android/beaconyx/yesdexproject/Activity/AttendInfoActivity.java
@@ -30,6 +30,16 @@ public class AttendInfoActivity extends FragmentActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_attend_info);
 
+        initView();
+
+
+        mDialogFragment1 = AttendDialogFragment1.newInstance();
+        mDialogFragment2 = AttendDialogFragment2.newInstance();
+
+    }//end onCreate
+
+    private void initView(){
+
         mLectureInfoListView = (StickyListHeadersListView) findViewById(R.id.lecture_info_list);
         mHeaderListViewAdapter = new HeaderListViewAdapter(getApplicationContext());
 
@@ -37,7 +47,7 @@ public class AttendInfoActivity extends FragmentActivity {
         HeaderListViewModel headerListViewModel2 = new HeaderListViewModel();
         HeaderListViewModel headerListViewModel3 = new HeaderListViewModel();
         HeaderListViewModel headerListViewModel4 = new HeaderListViewModel();
-
+//
         headerListViewModel1.setmHeaderid(1);
         headerListViewModel1.setmHeader("11월 10일 (금)");
         headerListViewModel1.setmEducatorName("강연자 : 최점일 교수");
@@ -46,48 +56,44 @@ public class AttendInfoActivity extends FragmentActivity {
 
         headerListViewModel1.setDemoEduImage(R.drawable.demo_header_img1);
         headerListViewModel1.setDemoStateImage(R.drawable.demo_state_img1);
-
+//
         headerListViewModel2.setmHeaderid(1);
         headerListViewModel2.setmHeader("11월 10일 (금)");
-        headerListViewModel2.setmEducatorName("강연자 :   손우성 교수");
-        headerListViewModel2.setmEducatorTime("강연 시간 : 14:00 ~ 15:00");
+        headerListViewModel2.setmEducatorName("강연자 : 손우성 교수");
+        headerListViewModel2.setmEducatorTime("강연 시간 : 15:00 ~ 16:00");
         headerListViewModel2.setmEducatorTitle("강연제목 : 치과의사의 직업전문성...");
 
         headerListViewModel2.setDemoEduImage(R.drawable.demo_header_img2);
         headerListViewModel2.setDemoStateImage(R.drawable.demo_state_img2);
-
+//
         headerListViewModel3.setmHeaderid(1);
         headerListViewModel3.setmHeader("11월 11일 (금)");
-        headerListViewModel3.setmEducatorName("강연자 : 손우성 교수, 이부규 교수, 박홍주 교수");
-        headerListViewModel3.setmEducatorTime("강연 시간 : 10:00 ~ 12:00");
-        headerListViewModel3.setmEducatorTitle("강연제목 : 보톡스 시술을 시작하기 위한 필수 강연");
+        headerListViewModel3.setmEducatorName("강연자 : 김복주 교수");
+        headerListViewModel3.setmEducatorTime("강연 시간 : 16:00 ~ 17:00");
+        headerListViewModel3.setmEducatorTitle("강연제목 : 실패한 임플란트를 극복하는 smart way");
 
         headerListViewModel3.setDemoEduImage(R.drawable.demo_header_img3);
-        headerListViewModel3.setDemoStateImage(R.drawable.demo_state_img3);
-
+        headerListViewModel3.setDemoStateImage(R.drawable.demo_state_img1);
+//
         headerListViewModel4.setmHeaderid(2);
-
         headerListViewModel4.setmHeader("11월 11일 (토)");
-        headerListViewModel4.setmEducatorName("강연자 : 손우성 교수, 이부규 교수, 박홍주 교수");
+        headerListViewModel4.setmEducatorName("강연자 : 김철훈 교수, 이부규 교수, 박홍주 교수");
         headerListViewModel4.setmEducatorTime("강연 시간 : 10:00 ~ 12:00");
         headerListViewModel4.setmEducatorTitle("강연제목 : 보톡스 시술을 시작하기 위한 필수 강연");
 
-        headerListViewModel4.setDemoEduImage(R.drawable.demo_header_img3);
+        headerListViewModel4.setDemoEduImage(R.drawable.demo_header_img4);
         headerListViewModel4.setDemoStateImage(R.drawable.demo_state_img3);
-
 
         mHeaderListViewAdapter.addList(headerListViewModel1);
         mHeaderListViewAdapter.addList(headerListViewModel2);
         mHeaderListViewAdapter.addList(headerListViewModel3);
+        mHeaderListViewAdapter.addList(headerListViewModel4);
 
         mLectureInfoListView.setAdapter(mHeaderListViewAdapter);
 
         mLectureInfoListView.setOnItemClickListener(onItemClickListener);
 
-        mDialogFragment1 = AttendDialogFragment1.newInstance();
-        mDialogFragment2 = AttendDialogFragment2.newInstance();
-
-    }//end onCreate
+    }
 
     @Override
     protected void onResume() {
--- app/src/main/java/com/android/beaconyx/yesdexproject/CustomView/CustomMapView.java
+++ app/src/main/java/com/android/beaconyx/yesdexproject/CustomView/CustomMapView.java
@@ -1,10 +1,6 @@
 package com.android.beaconyx.yesdexproject.CustomView;
 
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.widget.ImageView;
@@ -28,12 +24,6 @@ public class CustomMapView extends ImageView {
         super(context, attrs);
         this.setBackgroundResource(R.drawable.map_sample_img);
 
-        Bitmap marker = Bitmap.createBitmap(100, 100, Bitmap.Config.ARGB_8888);
-        Canvas canvas = new Canvas(marker);
-        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
-
-        paint.setColor(Color.BLACK);
-        canvas.drawCircle(50,50,10, paint);
     }
 
 
