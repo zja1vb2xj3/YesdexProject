6bdbf2b3e67f347305daaa0a8ee2bd17d1f12de3
박동관 <zja1vb2xj3@naver.com>
2017-10-26 10:54:18 +0900

    Account화면 추가

---
+++ app/src/main/java/com/android/beaconyx/yesdexproject/Activity/AccountActivity.java
@@ -0,0 +1,15 @@
+package com.android.beaconyx.yesdexproject.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.android.beaconyx.yesdexproject.R;
+
+public class AccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account);
+    }
+}
